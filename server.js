c0c09c1d4ef38c411ff4e05e7aa7618904a11a2f
Ahmed-Alanaswah <alanaswehahmed0@gmail.com>
2021-10-04 18:14:38 +0300

    practic

--- server.js
+++ server.js
@@ -18,11 +18,20 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
 app.use(express.json());
-mongoose.connect("mongodb://localhost:27017/flowers", {
-	useNewUrlParser: true,
+// mongoose.connect("mongodb://localhost:27017/flowers", {
+// 	useNewUrlParser: true,
 
-	useUnifiedTopology: true,
-});
+// 	useUnifiedTopology: true,
+// });
+
+mongoose.connect(
+	"mongodb+srv://Aa1791994:Aa1791994@cluster0.h4c2b.mongodb.net/Newflower?retryWrites=true&w=majority",
+	{
+		useNewUrlParser: true,
+
+		useUnifiedTopology: true,
+	}
+);
 app.listen(PORT, () => {
 	console.log(`server start with port ${PORT}`);
 });
